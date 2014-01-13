fbac54d7b687a86957dbdce1439c220810ca045e
wallyali <abdishwak@gmail.com>
2014-01-12 23:04:57 -0600

    Add modified user and application controllers and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Wikiful::Application.routes.draw do
-  resources :users
+  get 'signup', to: 'users#new', as: 'signup'
+  get 'login', to: 'sessions#new', as: 'login'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
 
   get "welcome/index"
   # The priority is based upon order of creation: first created -> highest priority.
@@ -10,6 +12,8 @@ Wikiful::Application.routes.draw do
    resources :articles
    resources :categories
    resources :users
+   resources :sessions
+  
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
